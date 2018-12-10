f7891a806433798f4638c28954a046e52b90b8c7
masterUNG <phrombutr@gmail.com>
2018-12-10 13:35:06 +0700

    Learning How to input Value

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
+  // Explicit
+  private categoryTitle: string;
+  private shopMember: string;
+
   constructor() { }
 
   ngOnInit() {
-  }
 
-}
+    this.categoryTitle = 'Section Category';
+
+  } // ngOnInit Function
+
+  addMember(memberString: string){
+
+    console.log('addMember Work');
+    this.shopMember = memberString;
+    alert('You Add ==> ' + this.shopMember);
+    
+  } // addMembed
+
+
+
+} // Main Class
